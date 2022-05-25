7b8b940b83a79702d63ebb0041b22a9afdfa58a1
Javier Moya <javiermoya81@hotmail.com>
2022-05-25 00:57:54 -0300

    Desafío Websocket

--- server.js
+++ server.js
@@ -1,34 +1,29 @@
 
 const express = require('express');
-const productosRouter = require('./routers/productosRouter');
-const handlebars = require('express-handlebars');
+const {Server: ioServer} = require('socket.io')
+const http = require('http')
 
 const server = express()
-const port = 8080
+const httpServer = http.createServer(server)
+const io = new ioServer(httpServer)
 
-server.engine(
-    "hbs",
-    handlebars.engine({
-        extname: ".hbs",
-        defaultLayout: "index.hbs",
-        layoutDir: __dirname + "/views/layouts",
-        partialsDir: __dirname + "/views/partials/"
-    })
-)
-
-server.set('view engine', 'hbs')
-server.set('views', "./views")
 server.use(express.json())
 server.use(express.urlencoded({extended:true}))
+server.use(express.static(__dirname+'/public'))
 
-server.get('/', (req, res)=>{
-    res.render('form')
-})
+const listaProductos = [];
 
-server.use('/', productosRouter);
+io.on('connection', function(socket) {
+    console.log('Un cliente se ha conectado' + socket.id);
 
+    socket.on('new-product',producto=>{
+        listaProductos.push(producto)
+        io.sockets.emit('new-list', listaProductos)
+    })
+});
 
-server.listen(port, ()=>{
+const port = 8080
+httpServer.listen(port, ()=>{
     console.log(`Servidor conectado al puerto ${port}`)
 })
 server.on("error",error=>console.log(`Se produjo error de servidor ${error}`))
